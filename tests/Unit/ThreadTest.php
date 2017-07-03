ab12e35c4a2cdb6256a56652c9aa934e2bca794d
eadande <dithe1993>
2017-07-03 10:43:33 +0300

    Threads assigned channels

--- tests/Unit/ThreadTest.php
+++ tests/Unit/ThreadTest.php
@@ -18,12 +18,17 @@ class ThreadTest extends TestCase
     public function setUp()
     {
         parent::setUp();
-        $this->thread = factory('App\Thread')->create();
+        $this->thread = create('App\Thread');
     }
 
     public function testExample()
     {
         $this->assertTrue(true);
+    }
+    /** @test */
+    function a_thread_can_make_a_string_path()
+    {
+        $this->assertEquals("/threads/{$this->thread->channel->slug}/{$this->thread->id}", $this->thread->path());
     }
     /** @test */
     function a_thread_has_replies()
@@ -44,5 +49,11 @@ class ThreadTest extends TestCase
             'user_id' => 1
         ]);
         $this->assertCount(1, $this->thread->replies);
+    }
+    /** @test */
+    public function a_thread_belongs_to_a_channel()
+    {
+        $thread = create('App\Thread');
+        $this->assertInstanceOf('App\Channel', $thread->channel);
     }
 }

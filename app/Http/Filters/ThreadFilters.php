8ff972a7047e4862004a13cf7cd8f8e8ef166be4
eadande <dithe1993>
2017-07-13 09:50:55 +0300

    Filter by popularity for all threads

--- app/Http/Filters/ThreadFilters.php
+++ app/Http/Filters/ThreadFilters.php
@@ -6,7 +6,7 @@ use App\User;
 
 class ThreadFilters extends Filters
 {
-    protected $filters = ['by'];
+    protected $filters = ['by', 'popular'];
     /**
      * Filter the query by a given username
      * @param string $username
@@ -16,5 +16,16 @@ class ThreadFilters extends Filters
     {
         $user = User::where('name', $username)->firstOrFail();
         return $this->builder->where('user_id', $user->id);
+    }
+
+    /**
+     * Filter query according to most popular threads
+     *
+     * @return mixed
+     */
+    protected function popular()
+    {
+        $this->builder->getQuery()->orders = [];
+        return $this->builder->orderBy('replies_count', 'desc');
     }
 }

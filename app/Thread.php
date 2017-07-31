5bcc737d097cc7dbaf5ea0731133ea1ef9fa2e8a
eadande <dithe1993>
2017-08-01 01:43:07 +0300

    Optimising database queries Part 2

--- app/Thread.php
+++ app/Thread.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Thread extends Model
 {
     protected $guarded = [];
+    protected $with = ['creator', 'channel'];
 
     protected static function boot()
     {
@@ -30,9 +31,9 @@ class Thread extends Model
      */
     public function replies()
     {
-        return $this->hasMany(Reply::class)
-            ->withCount('favorites')
-            ->with('owner');
+        return $this->hasMany(Reply::class);
+//            ->withCount('favorites')
+//            ->with('owner');
     }
 
     /**

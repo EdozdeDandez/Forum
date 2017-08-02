8e633ffee1cb050d1b0f417f45d274e811e1c8c5
eadande <dithe1993>
2017-08-03 00:08:10 +0300

    Creating an activity feed Part 1

---
+++ app/RecordsActivity.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: eadande
+ * Date: 8/2/17
+ * Time: 10:53 PM
+ */
+
+namespace App;
+
+
+use Illuminate\Support\Facades\App;
+
+trait RecordsActivity
+{
+
+    protected static function bootRecordsActivity()
+    {
+        if (auth()->guest()) return;
+
+        foreach(static::getActivitiesToRecord() as $event){
+            static::$event(function ($model) use ($event){
+                $model->recordActivity($event);
+            });
+        }
+    }
+    public static function getActivitiesToRecord()
+    {
+        return ['created'];
+    }
+    public function getActivityType($event)
+    {
+        $type = strtolower((new \ReflectionClass($this))->getShortName());
+        return  "{$event}_{$type}";
+    }
+
+    protected function recordActivity($event)
+    {
+        $this->activity()->create([
+            'user_id' => auth()->id(),
+            'type' => $this->getActivityType($event),
+        ]);
+    }
+    public function activity()
+    {
+        return $this->morphMany('App\Activity', 'subject');
+    }
+}
